f6dd68017174aeae6502ae034e71592e0e67fabf
Ankith Konda <ankithkonda@ankiths-mbp.gateway>
2014-07-18 18:54:16 +1000

    Initial Commit

---
+++ eta/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  eta
+//
+//  Created by Ankith Konda on 7/18/14.
+//  Copyright (c) 2014 Ankith Konda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ eta/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  eta
+//
+//  Created by Ankith Konda on 7/18/14.
+//  Copyright (c) 2014 Ankith Konda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
